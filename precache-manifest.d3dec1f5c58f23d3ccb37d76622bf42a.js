9a0032a1f21271616253e3a2c4fc692ecfcfab35
Nhat Ho <honhattan2411@gmail.com>
2020-07-29 20:24:07 -0700

    Updates

---
+++ precache-manifest.d3dec1f5c58f23d3ccb37d76622bf42a.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f59da02badcc25a94ab1a73c2393b9da",
+    "url": "/React1_CovidTracker/index.html"
+  },
+  {
+    "revision": "9d0a1e7d44437cf412ba",
+    "url": "/React1_CovidTracker/static/css/main.f281cd2a.chunk.css"
+  },
+  {
+    "revision": "66021c561ef01f23c072",
+    "url": "/React1_CovidTracker/static/js/2.9c223864.chunk.js"
+  },
+  {
+    "revision": "176852ae27885dfd7559e0fb4b332d24",
+    "url": "/React1_CovidTracker/static/js/2.9c223864.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9d0a1e7d44437cf412ba",
+    "url": "/React1_CovidTracker/static/js/main.4b4ce70f.chunk.js"
+  },
+  {
+    "revision": "5b0d58f2ca9bfa3f78e6",
+    "url": "/React1_CovidTracker/static/js/runtime-main.6bad4594.js"
+  },
+  {
+    "revision": "8cfd29513aee6280f2b5697a795fb309",
+    "url": "/React1_CovidTracker/static/media/Covid_Banner_1024x259.8cfd2951.jpg"
+  }
+]);
